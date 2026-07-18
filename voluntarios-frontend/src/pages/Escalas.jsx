7dc32cce8b439047b08d7c165c4476e059dfd3d3
Kevyn Targino <kevyntargino@gmail.com>
2026-07-17 20:24:02 -0400

    melhorias de teste

--- voluntarios-frontend/src/pages/Escalas.jsx
+++ voluntarios-frontend/src/pages/Escalas.jsx
@@ -1453,9 +1453,8 @@ function LiveBadge() {
       <span className="relative flex h-2.5 w-2.5">
         <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-500 opacity-75" />
         <span className="relative inline-flex h-2.5 w-2.5 rounded-full bg-red-600" />
-      </span>
-      <Radio size={14} className="animate-pulse" />
-      Ao vivo
+      </span> 
+      {/* ao vivo */}
     </span>
   );
 }
